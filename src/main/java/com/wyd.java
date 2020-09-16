f9a87ae3fe484582ee6d810c33cdef0f9877fc62
c9dz7 <1049857763@qq.com>
2020-09-16 16:43:26 +0800

    Initial commit

---
+++ src/main/java/com/wyd.java
@@ -0,0 +1,7 @@
+package com;
+
+public class wyd {
+    public  void main() {
+        System.out.println("123");
+    }
+}
---
+++ src/test/java/com/wydTest.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class wydTest {
+
+    @Test
+    public void main() {
+        System.out.println("546");
+    }
+}
